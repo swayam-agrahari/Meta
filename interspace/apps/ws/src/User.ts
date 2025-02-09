c007f54d0b5b7bf41caf6cf22e278864191e10a7
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-02-10 00:22:19 +0530

    fix:some movement logic

--- interspace/apps/ws/src/User.ts
+++ interspace/apps/ws/src/User.ts
@@ -87,16 +87,24 @@ export class User {
                     this.x = Math.floor(Math.random() * space.width);
                     this.y = Math.floor(Math.random() * space.height);
                     RoomManager.getInstance().addUser(spaceId, this);
+
                     this.send({
                         type: "space-joined",
                         payload: {
-                            spawn: {  // Fix typo here
+                            spawn: {
                                 x: this.x,
                                 y: this.y
-                            }
+                            },
+                            userId: this.userId,
                         },
                         users: RoomManager.getInstance().rooms.get(spaceId)?.filter(x => x.id != this.id).map((u) => {
-                            return { id: u.id };  // Correctly return the object with the `id` property
+                            console.log("userid" + u.userId + " for this user " + u.id)
+                            return {
+                                id: u.userId,
+                                x: u.x,
+                                y: u.y,
+
+                            };  // Correctly return the object with the `id` property
                         }) ?? []  // Fallback to an empty array if no users are present
                     })
 
@@ -114,16 +122,19 @@ export class User {
                 case "move":
                     const x = parsedMessage.payload.x
                     const y = parsedMessage.payload.y
+                    console.log("user ley move pathayo", x, y)
 
                     const xDisplacement = Math.abs(this.x - x)
                     const yDisplacement = Math.abs(this.y - y)
-                    if (((xDisplacement == 1) && (yDisplacement == 0)) || ((xDisplacement == 0) && (yDisplacement == 1))) {
+                    console.log("xDisplacement " + xDisplacement + " yDisplacement" + yDisplacement)
+                    if (Math.abs(this.x - x) <= 5 && Math.abs(this.y - y) <= 5) {
                         this.x = x;
                         this.y = y;
+                        console.log("id of user that send move is" + this.userId)
                         RoomManager.getInstance().broadcast(this.spaceId!, this, {
                             type: "move",
                             payload: {
-                                id: this.id,
+                                userId: this.userId,
                                 x: this.x,
                                 y: this.y
                             }
