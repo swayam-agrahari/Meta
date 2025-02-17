18cadd125611d30619160e199d481874be8de098
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-02-17 13:15:48 +0530

    feat:added cors

--- interspace/apps/http/src/index.ts
+++ interspace/apps/http/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import { router } from './routes/v1'
-
+var cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 
 app.use("/api/v1", router)
 
