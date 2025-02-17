24a6a65ad271ab312e00c54e8230e036e4cd9e86
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-02-17 13:16:21 +0530

    feat:added components and other next setup

--- interspace/apps/frontend/next.config.ts
+++ interspace/apps/frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // In production, specify your Vercel domain
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Accept" },
+        ],
+
+      },
+    ];
+  },
 };
 
 export default nextConfig;
